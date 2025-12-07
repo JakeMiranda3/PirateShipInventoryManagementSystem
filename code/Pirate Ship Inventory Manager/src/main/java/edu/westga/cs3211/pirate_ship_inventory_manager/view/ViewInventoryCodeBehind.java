249375ea7a319d98e00c65ec1fcdc18d50f3e553
fmensah1 <fmensah1@my.westga.edu>
2025-12-07 11:26:16 -0500

    Updated Inventory class
    Updated the Inventory class  with logic to hold and manage a list of stock items. For now I have only created Inventory2 with my changes to avoid any merege conficts I'll go to add more fuctionality and later we can merge inventory and inventory2 carefully to avoid duplicates or loss of fucntionality

---
+++ code/Pirate Ship Inventory Manager/src/main/java/edu/westga/cs3211/pirate_ship_inventory_manager/view/ViewInventoryCodeBehind.java
@@ -0,0 +1,31 @@
+package edu.westga.cs3211.pirate_ship_inventory_manager.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+public class ViewInventoryCodeBehind {
+
+    @FXML
+    private ComboBox<?> compartmentComboBox;
+
+    @FXML
+    private ListView<?> inventoryListVIew;
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private ComboBox<?> specialQualityComboBox;
+
+    @FXML
+    private ComboBox<?> stockTypeComboBox;
+
+    @FXML
+    void handleHome(ActionEvent event) {
+
+    }
+
+}
